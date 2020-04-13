e10d8a5583bb5758aa050f013c4a9e685e5ebd0f
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2020-04-12 21:20:31 -0500

    Created tictac.py
    Started implementation of tictac AI with hill climbing

---
+++ tictacHillClimbing.py
@@ -0,0 +1,38 @@
+tictac = [0,0,0,0,0,0,0,0,0]
+
+#prints board in console
+def printBoard(arr):
+    columns = []
+    line1 = arr[0:3]
+    line2 = arr[3:6]
+    line3 = arr[6:9]
+    columns.append(line1)
+    columns.append(line2)
+    columns.append(line3)
+    for i in range(0, len(columns)):
+        print(columns[i])
+
+#checks if there is a winner to leave loop
+def checkForWinner(arr):
+    
+    #check horizontally
+    for i in range(0,len(arr),3):
+        if arr[i] == arr[i+1] and arr[i] == arr[i+2] and arr[i+1] == arr[i+2]:
+            return True
+
+    #check vertically
+    for i in range(0,3):
+        if arr[i] == arr[i+3] and arr[i] == arr[i+6] and arr[i+3] == arr[i+6]:
+            return True
+            
+    #check diagnolaly
+    for i in range(0,3,2):
+        if i == 0:
+            if arr[i] == arr[i+4] and arr[i] == arr[i+8] and arr[i+4] == arr[i+8]:
+                return True
+        
+        if i == 2:
+            if arr[i] == arr[i+2] and arr[i] == arr[i+4] and arr[i+2] == arr[i+4]:
+                return True
+
+checkForWinner(tictac)
