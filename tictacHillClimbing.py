7c4da657b2a503f2aae0e15a87e883837df8aabc
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2020-04-13 15:55:22 -0500

    started find f function

--- tictacHillClimbing.py
+++ tictacHillClimbing.py
@@ -14,12 +14,41 @@ def printBoard(arr):
 
 #checks if there is a winner to leave loop
 def checkForWinner(arr):
-    
     #check horizontally
     for i in range(0,len(arr),3):
         if arr[i] == arr[i+1] and arr[i] == arr[i+2] and arr[i+1] == arr[i+2]:
             return True
 
+    #check vertically
+    for i in range(0,3):
+        if arr[i] == arr[i+3] and arr[i] == arr[i+6] and arr[i+3] == arr[i+6]:
+            return True
+            
+    #check diagnolaly
+    for i in range(0,3,2):
+        if i == 0:
+            if arr[i] == arr[i+4] and arr[i] == arr[i+8] and arr[i+4] == arr[i+8]:
+                return True
+        
+        if i == 2:
+            if arr[i] == arr[i+2] and arr[i] == arr[i+4] and arr[i+2] == arr[i+4]:
+                return True
+
+def findf(arr, char):
+    f = 0
+
+    #check horizontally
+    for i in range(0,len(arr),3):
+        aux = 0
+        if arr[i] == char:
+            aux = aux + 1
+        if arr[i+1] == char:
+            aux = aux + 1   
+        if arr[i+2] == char:
+            aux = aux + 1 
+        if aux > f:
+            f = aux
+            
     #check vertically
     for i in range(0,3):
         if arr[i] == arr[i+3] and arr[i] == arr[i+6] and arr[i+3] == arr[i+6]:
